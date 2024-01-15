04005cf74ec9a11f2b0e8dd0b622b72a8a798044
Levangul <150108077+Levangul@users.noreply.github.com>
2024-01-15 18:52:37 -0500

    creating storage

--- Assets/script.js
+++ Assets/script.js
@@ -1,16 +1,3 @@
-
-//* GIVEN I am taking a code quiz
-//* WHEN I click the start button
-//* THEN a timer starts and I am presented with a question
-//* WHEN I answer a question
-//* THEN I am presented with another question
-//* WHEN I answer a question incorrectly
-//* THEN time is subtracted from the clock
-//* WHEN all questions are answered or the timer reaches 0
-//* THEN the game is over
-//* WHEN the game is over
-//* THEN I can save my initials and my score
-
 let timerEl = document.getElementById('timeleft')
 let startButton = document.getElementById('start-button')
 let quizPage = document.getElementById('quiz-page')
@@ -30,18 +17,12 @@ let points = 0
 let submitScore = document.getElementById('submit-score')
 let highScoreList = document.getElementById('list')
 let initials =  document.getElementById('name')
-
-submitScore.addEventListener("click", topScoresPage )
-function topScoresPage()  {
-    quizEnd.style.display = "none"
-    scoresPage.style.display = "flex" 
-    highScoreList.textContent =initials.value + " - " + points + " points"
-
-} 
+let clear = document.getElementById('clearscores')
+let back = document.getElementById('go-back')
+let topList = document.getElementById('lists')
 
 
-
-
+//* object with arrays of questions answers and correct answers
 
 let questions = [
     {
@@ -75,64 +56,28 @@ let questions = [
         correct: "C. Jazz-Funk"
     },
     {
+        question: "Who composed soundtrack for movie Gladiator ?",
+        choices: ["A. John Williams", "B. Hans Zimmer", "C. Elton John", "D. Danny Elfman "],
+        correct: "B. Hans Zimmer"
+    },
+    {
         question: "Who was the lead singer of Queen ?",
         choices: ["A. John Stewart", "B. Freddie Mercury", "C. Elton John", "D. Ozzy Osbourne "],
         correct: "B. Freddie Mercury"
     }
 ];
-    
 
-function showQuestion () {
-    questionEl.textContent = questions[questionIndex].question
-    answerBtn1.textContent = questions[questionIndex].choices[0];
-    answerBtn2.textContent = questions[questionIndex].choices[1];
-    answerBtn3.textContent = questions[questionIndex].choices[2];
-    answerBtn4.textContent = questions[questionIndex].choices[3];
-}
-function checkAnswer(event) {
-    
-    if (event.target.matches("button") ) 
-    
-    {  
-    if (questions[questionIndex].correct === event.target.textContent) {
-        check.textContent = "that's right!";
-        points = points +1;
-
-    } else {timeLeft = timeLeft -5; 
-        check.textContent = "Wrong! correct answer is " + questions[questionIndex].correct}
-    
-    
-        showQuestion(questionIndex++); 
-        
-        if (questionIndex +1 == questions.length) {
-            
-            scorePage();
-            //* saveScore();
-            timeLeft = 0
-        } 
-        console.log(points)
-}      
-} 
-
-answerButtons.addEventListener("click", checkAnswer);
+//* event listener for start button
 
 startButton.addEventListener("click", function() {
+//* setting main pages display to none to see only quiz page
     mainPage.style.display = "none"
     quizPage.style.display = "flex"
     countdown();
     showQuestion();
 } )
 
-function scorePage() {
-    
-    mainPage.style.display = "none"
-    quizPage.style.display = "none"
-    quizEnd.style.display = "flex"
-    quizEnd.children[0].textContent = 'Your final score is: ' + points
-    
-    
-    
-}
+//* setting timer
 
 let timeLeft = 40;
 function countdown() {
@@ -146,9 +91,113 @@ function countdown() {
         clearInterval(timeInterval);
         timerEl.textContent = "Time: 0"
         scorePage()
-        //* saveScore(); have to create
-        
         
     }
     }, 1000);
 }
+
+
+//* function to show each question
+
+function showQuestion () {
+    questionEl.textContent = questions[questionIndex].question
+    answerBtn1.textContent = questions[questionIndex].choices[0];
+    answerBtn2.textContent = questions[questionIndex].choices[1];
+    answerBtn3.textContent = questions[questionIndex].choices[2];
+    answerBtn4.textContent = questions[questionIndex].choices[3];
+}
+
+//* question one after each, adding points, reducing time and checking questions if correct
+
+function checkAnswer(event) {
+    
+    if (event.target.matches("button") ) 
+    
+    {  
+    if (questions[questionIndex].correct === event.target.textContent) {
+        check.textContent = "that's right!";
+        points = points +6;
+
+    } else {timeLeft = timeLeft -7; 
+        check.textContent = "Wrong! correct answer is " + questions[questionIndex].correct}
+    
+    
+        showQuestion(questionIndex++); 
+        
+        if (questionIndex +1 == questions.length) {
+            
+            scorePage();
+            timeLeft = 0
+        } 
+}   
+} 
+
+answerButtons.addEventListener("click", checkAnswer);
+
+
+//* end-page
+
+function scorePage() {
+    
+
+    mainPage.style.display = "none"
+    quizPage.style.display = "none"
+    quizEnd.style.display = "flex"
+    quizEnd.children[0].textContent = 'Your final score is: ' + points   
+}
+
+
+
+//* function for submit button
+
+submitScore.addEventListener("click", function() {
+    if (initials.value === "") {
+        alert("please enter your name") 
+        return 
+    } else {
+    topScoresPage() } 
+} )
+
+function topScoresPage()  {
+    quizEnd.style.display = "none"
+    scoresPage.style.display = "flex" 
+    //*local storage to keep information
+    let storedInfo = {
+        initials: initials.value,
+        score: points}
+
+    localStorage.setItem("storedInfo", JSON.stringify(storedInfo))
+    let renderInfo = JSON.parse(localStorage.getItem("storedInfo"))
+
+
+    //* creating list to hold information and show on the page
+    for (i = 0; i > initials.index; i++) {
+
+    let li = document.createElement ("li")
+    li.textContent = renderInfo.initials + " - " + renderInfo.score + "points"
+
+    //* adding to parent element
+
+    topList.appendChild(li); } 
+    console.log(initials.index)
+    }
+    
+    
+
+viewScores.addEventListener("click", function()  {
+    mainPage.style.display = "none"
+    quizPage.style.display = "none"
+    quizEnd.style.display = "none"
+    topScoresPage()
+} )
+
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
+
+back.addEventListener("click", function() {
+    window.location.reload() } )
+    
+
+    
